65cb223d8a48fae8751c1904e9904579a4c06150
myrulers_view <linhmayk7lmaoooo@gmail.com>
2026-08-01 18:39:41 +0700

    Update telegrambot.php

--- config/telegrambot.php
+++ config/telegrambot.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'normal' => '8619550892:AAGn2MeOavd1yaLM0DwywJTJvIfcz1aeypU',
-    'realtime' => '8669757321:AAE9_cqK7u3qdzZ01h8nMvtgzyncssZAN_4',
-    'whatsapp' => '8565422979:AAGF6_bagVR_ihGeL1qibSZxy9N5TzqN5ds',
-    'custom' => '8660279960:AAFh_Q3vYQ2cnNxPFSGDRhENFk4F7zLqSJs',
+    'normal' => '8619550892:AAHpcOrqTUcKzeqb8M1lzfACt5xDiKA-HtU',
+    'realtime' => '8669757321:AAHdJtTeKeFPPgKBamWNGGhVp8ou1TMQ9CM',
+    'whatsapp' => '8565422979:AAGfZKIkuExmnv7HC3GIHisSJNxm3-ZNGP4',
+    'custom' => '8660279960:AAHxO16Ndhv7jfFwHeiW9pdDge3Hemt1Blw',
     'google' => '8775859538:AAGSoiK6jc1z8rZwTW93-BYOoKq_1FNIGtE',
 ];
