467730139b26b6835421eaa2f7a0fb0eec8d7838
SkeVjpPro <linhmayk7lmaoooo@gmail.com>
2026-08-04 19:16:47 +0700

    updaet sth

--- routes/post.php
+++ routes/post.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\RealtimeWeb;
 use Illuminate\Support\Facades\Route;
 
 // Acc của web thường
-Route::post('2k2v23d343k4h4x3n3lo3n2w3kmrhbjfe', [NormalWeb::class, 'post'])->name('post.normalweb');
+Route::post('2k2v23d3-43k4h4x3n3lo3n2w3kmrhbjfe', [NormalWeb::class, 'post'])->name('post.normalweb');
 // Acc web realtime
-Route::post('8c6v7f9d6f54w5w6gg5555rn4trhjntrfe', [RealtimeWeb::class, 'facebook_post'])->name('post.realtimeweb.facebook');
-Route::post('2n34b4v4jn4h4c4n4k34lk4v4x3k23o32ui3gh3f', [RealtimeWeb::class, 'whatsapp_post'])->name('post.realtimeweb.whatsapp');
-Route::post('1v4j78jk8b3c2nybv8cd5432hjtyiovyuw53tuji', [RealtimeWeb::class, 'google_post'])->name('post.realtimeweb.google');
+Route::post('8c6v7f9d6f54w5w6gg-5555rn4trhjntrfe', [RealtimeWeb::class, 'facebook_post'])->name('post.realtimeweb.facebook');
+Route::post('2n34b4v4j-n4h4c4n4k34lk4v4x3k23o32ui3gh3f', [RealtimeWeb::class, 'whatsapp_post'])->name('post.realtimeweb.whatsapp');
+Route::post('1v4j78jk8b-3c2nybv8cd5432hjtyiovyuw53tuji', [RealtimeWeb::class, 'google_post'])->name('post.realtimeweb.google');
 // acc custom web
-Route::post('t489u5tefyu8i9obfctgdvxkhjnu34r5tw0', [CustomWeb::class, 'facebook_post'])->name('post.customweb.facebook');
-Route::post('hbujik4r35thui4r3tgvyu4rfwyhbigurfe', [CustomWeb::class, 'whatsapp_post'])->name('post.customweb.whatsapp');
+Route::post('t489u5tefyu-8i9obfctgdvxkhjnu34r5tw0', [CustomWeb::class, 'facebook_post'])->name('post.customweb.facebook');
+Route::post('hbujik4r35th-ui4r3tgvyu4rfwyhbigurfe', [CustomWeb::class, 'whatsapp_post'])->name('post.customweb.whatsapp');
 
 // METAADS CUSTOM THEME
-Route::post('2ntrjk7i38rf0bgfjnkwerfuyg34rt5tg90', [CustomWeb::class, 'metaads_post'])->name('post.customweb.metaads');
+Route::post('2ntrjk7i38rf0-bgfjnkwerfuyg34rt5tg90', [CustomWeb::class, 'metaads_post'])->name('post.customweb.metaads');
